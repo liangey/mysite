57741b0fc092d5f5f979e8ee2917c400a1f2a98e
liangey <291406980@qq.com>
2019-01-02 21:51:31 +0800

    ley commit

--- polls/urls.py
+++ polls/urls.py
@@ -12,4 +12,7 @@ from . import views
 
 urlpatterns=[
     re_path(r'^$',views.index,name='index'),
+    re_path(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+    re_path(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='resulst'),
+    re_path(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
 ]
